9660f25197d20cd68ab8f1054a0599b9b994b68e
veda.ni <vedaprakash.n@gmail.com>
2024-08-20 23:48:44 +0530

    consignment details changes

---
+++ src/main/java/freezy/dto/v1/GoodsDetailDTO.java
@@ -0,0 +1,86 @@
+package freezy.dto.v1;
+
+public class GoodsDetailDTO {
+
+    String productId;
+    Integer quantity;
+    Integer unitPrice;
+    String iduSerialNo;
+    String oduSerialNo;
+    String type;
+    String accessoryId;
+    String serviceId;
+    Double tax;
+
+    public Double getTax() {
+        return tax;
+    }
+
+    public void setTax(Double tax) {
+        this.tax = tax;
+    }
+
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(String serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAccessoryId() {
+        return accessoryId;
+    }
+
+    public void setAccessoryId(String accessoryId) {
+        this.accessoryId = accessoryId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Integer unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getIduSerialNo() {
+        return iduSerialNo;
+    }
+
+    public void setIduSerialNo(String iduSerialNo) {
+        this.iduSerialNo = iduSerialNo;
+    }
+
+    public String getOduSerialNo() {
+        return oduSerialNo;
+    }
+
+    public void setOduSerialNo(String oduSerialNo) {
+        this.oduSerialNo = oduSerialNo;
+    }
+}
